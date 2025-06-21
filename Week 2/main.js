e9f24b063df056e00c62342bb9ad7daa2dcba2ea
keroles michael <kerolesmichael1@gmail.com>
2025-06-21 20:12:53 +0300

    Week 2 And challenge

---
+++ Week 2/main.js
@@ -0,0 +1,36 @@
+// Task #01
+// let numberOne = 10;
+// let numberTwo = 20;
+
+// console.log(numberOne + "" + numberTwo);
+// console.log(typeof (numberOne + "" + numberTwo));
+// console.log(`${numberOne}${numberTwo}`);
+// console.log(typeof `${numberOne}${numberTwo}`);
+
+// console.log(numberTwo + "\n" + numberOne);
+
+// console.log(`${numberTwo}
+// ${numberOne}`);
+
+// Task #02
+// console.log(elzero.innerHTML); // object
+// console.log(typeof elzero); // object
+
+// Task #03
+// console.log(
+//   '`I\'m In \n\\\\\nLove \\\\ "" \'\'\'\n++ With ++\n\\"""\\"""\n""JavaScript""``'
+// );
+
+//Task #04
+// let a = 21;
+// let b = 20;
+// console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`); // _21_2021_2021_2021_20_
+
+// challenge
+// let title = "Elzero",description = "Elzero Web School", date = "25/10";
+// let result = `<div class="card">
+//   <h3>Hello ${title}</h3>
+//   <p>${description}</p>
+//   <span>${date}</span>
+//   </div>`;
+// document.write(result.repeat(4));
