2e77c7ff4fe14e5066aab1c02c488a33357dd9f9
keroles michael <kerolesmichael1@gmail.com>
2025-06-21 19:44:27 +0300

    Week one

---
+++ Week 1/main.js
@@ -0,0 +1,38 @@
+// Task #02
+// document.write("<h1>Elzero</h1>");
+// document.querySelector("h1").style.color = "blue";
+// document.querySelector("h1").style.fontSize = "80px";
+// document.querySelector("h1").style.fontWeight = "bold";
+// document.querySelector("h1").style.textAlign = "center";
+// document.querySelector("h1").style.fontFamily = "arial";
+
+// Task #03
+// console.log(
+//   "%cElzero %cWeb %cSchool",
+//   "color:red; font-size:40px",
+//   "color:green;font-weight:bold; font-size:40px",
+//   "color:white; font-size:40px;background-color:blue"
+// );
+
+// Task #04
+// console.group("Group 1");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.group("Child Group");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.group("Grand Child Group");
+// console.log("Message 1");
+// console.log("Message 2");
+// console.groupEnd();
+// console.groupEnd();
+// console.group("Group 2");
+// console.log("Message 1");
+// console.log("Message 2");
+
+//Task #05
+// console.table(["Keroles", "Michael", "ALi", "Sameh"]);
+
+//Task 06
+// console.log("Iam In Console");
+// document.write("Iam In Page");
